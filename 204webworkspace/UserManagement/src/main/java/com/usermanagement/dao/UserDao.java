3ad986936c7835cbf578d3204aa8349926106d2e
jiaying <vison919@gmail.com>
2022-05-06 16:57:28 +0800

    2022-05-06

--- 204webworkspace/UserManagement/src/main/java/com/usermanagement/dao/UserDao.java
+++ 204webworkspace/UserManagement/src/main/java/com/usermanagement/dao/UserDao.java
@@ -2,4 +2,8 @@ package com.usermanagement.dao;
 
 public class UserDao {
 
+	private static final String DB_URL = 
+					"jdbc:sqlserver://localhost:1433;databaseName=jdbc";
+	private static final String USER = "sa";
+	private static final String PASSWORD = "passw0rd";
 }
